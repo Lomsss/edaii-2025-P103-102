ce4347fd5b6f5f40091116bc75e1cff7b0817433
Dan <brad.dan01@estudiant.upf.edu>
2025-05-18 14:36:30 +0200

    LAB3

--- src/main.c
+++ src/main.c
@@ -4,7 +4,16 @@
 #define DATASET_SIZE 12
 
 int main() {
+  /*
   load_all_documents("wikipedia12", DATASET_SIZE);
   printf("What document do you want to print? (ID range 0-%d): ", DATASET_SIZE);
+  */
+  DocumentList docs = load_all_documents("wikipedia12", DATASET_SIZE);
+  HashMap *index = reverse_index(&docs, 1000);
+  search_term(index, "cat");
+
+  DocumentListFree(&docs);
+  hashmap_free(index); 
+
   return 0;
 }
